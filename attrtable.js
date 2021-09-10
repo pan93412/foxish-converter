98f51caf90d5e80865223dea24cfbe091d8ad729
Yi-Jyun Pan <pan93412@gmail.com>
2021-09-10 20:17:36 +0800

    feat(attrtable): add more rules

--- attrtable.js
+++ attrtable.js
@@ -1,4 +1,10 @@
 /** @type {[RegExp, string][]} */
 export const attr = [
-    [/nevermind/g, "nvm"],
+    [/nevermind/ig, "nvm"],
+    [/I wil|Il/ig, "I'll"],
+    [/I have/ig, "I'd"],
+    [/(aw)(o+)/ig, "$1$2$2$2$2$2$2"],
+    [/You/ig, "u"],
+    [/Are/ig, "re"],
+    [/\s(a|an|the)\s/g, " "],
 ];
